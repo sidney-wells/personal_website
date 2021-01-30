52290641581c00505affd3d8293f083083a172ee
Sidney <sidneywells101@gmail.com>
2021-01-30 11:09:40 -0600

    test setup

---
+++ __tests__/components/text.test.js
@@ -0,0 +1,8 @@
+import { Text } from '../../src/components';
+import { render, screen } from '../../test-utils';
+
+describe('Text', () => {
+  it('should render text', () => {
+    render(<Text />);
+  });
+});
---
+++ test-utils.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'theme-ui';
+import theme from './src/theme';
+import userEvent from '@testing-library/user-event';
+
+const Providers = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: Providers, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+export { customRender as render };
+export { userEvent };
