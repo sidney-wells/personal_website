69f4bd248bedf003a1fc317ab89e949027632878
Sidney Wells <sidneywells101@gmail.com>
2021-12-23 19:41:35 -0600

    initial commit

---
+++ old/__tests__/pages/projects.test.js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+import { render } from '../../test-utils';
+import Project from '../../../src/pages/projects';
+
+jest.mock('next/router');
+
+describe('Project', () => {
+    let pathname;
+
+    it('should render the project page', () => {
+        // pathname = '/projects';
+        // useRouter.mockImplementation(() => ({
+        //   pathname
+        // }));
+        render( < Project / > );
+    });
+});
