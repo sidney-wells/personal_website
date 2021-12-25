caf8b620d83ec7dae7134390289efc9dc68561d1
Sidney Wells <sidney@Sidneys-MacBook-Air.local>
2021-12-25 11:54:29 -0600

    move files to src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import TextField from '../learnings/components/TextField'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <TextField text="S"/>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
